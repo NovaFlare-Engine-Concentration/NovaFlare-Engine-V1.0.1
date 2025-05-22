ca5454e99695fbf140d1e9566f54e204f7881b60
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-05-22 16:41:25 +0000

    macro

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -14,8 +14,9 @@ import haxe.CallStack;
 import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
+#if !macro
 import flash.system.System;
-
+#end
 /**
  * ...
  * @author: Saw (M.A. Jigsaw)
