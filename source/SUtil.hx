48cd38945527851fb0fd3743ad7011e96caca29f
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-05-22 19:30:28 +0000

    tgt

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -14,9 +14,8 @@ import haxe.CallStack;
 import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
-#if !macro
-import flash.system.System;
-#end
+import lime.system.System;
+
 /**
  * ...
  * @author: Saw (M.A. Jigsaw)
